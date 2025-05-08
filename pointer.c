9fec7be83cd89f8d70981f102589871b4c3aefa0
Tamim Iqbal Maruf <marufmd775@gmail.com>
2025-05-08 22:21:19 +0600

    print stack store

--- pointer.c
+++ pointer.c
@@ -4,6 +4,15 @@ int main()
 {
     int x = 10;
     // printf("%d", x);
-    printf("%p", &x);
+    printf("%p\n", &x);
+
+    int* ptr;
+
+    ptr = &x;
+
+    printf("%p\n", ptr);
+
+    printf("%p\n", &ptr);
+
     return 0;
 };
