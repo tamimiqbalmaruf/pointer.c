69a814700cac4447a93bdd1041b8bc8e834e6374
Tamim Iqbal Maruf <marufmd775@gmail.com>
2025-05-09 05:29:57 +0600

    Dereferencing a pointer

--- pointer.c
+++ pointer.c
@@ -3,16 +3,18 @@
 int main()
 {
     int x = 10;
-    // printf("%d", x);
+    printf("%d\n", x);
     printf("%p\n", &x);
 
     int* ptr;
 
     ptr = &x;
 
+    *ptr = 20;
+
     printf("%p\n", ptr);
 
-    printf("%p\n", &ptr);
+    printf("%d\n", *ptr);
 
     return 0;
 };
