75a2ee143793ce63c78e8df7d4e4b6345fcebb89
Tamim Iqbal Maruf <marufmd775@gmail.com>
2025-05-09 12:04:32 +0600

    Pattern 2 done

--- assignment-2.c
+++ assignment-2.c
@@ -2,6 +2,28 @@
 
 int main()
 {
-    
+    int n, value = 1;
+
+    scanf("%d", &n);
+
+    int space = n - 1;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= space; j++)
+        {
+            printf(" ");
+        }
+
+        for (int k = value; k > 0; k--)
+        {
+            printf("%d", k);
+        }
+
+        printf("\n");
+        value++;
+        space--;
+    }
+
     return 0;
 };
